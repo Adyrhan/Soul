8070b83b1c7b8510c5e4ce04917631db5a5dfca7
Adrian Perez Heredia <aperezhrd@gmail.com>
2016-03-17 20:26:42 +0100

    Implemented FileUtils.isSymlink() method.

---
+++ app/src/main/java/com/adyrsoft/soul/utils/FileUtils.java
@@ -0,0 +1,22 @@
+package com.adyrsoft.soul.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by Adrian on 17/03/2016.
+ */
+public class FileUtils {
+    public static boolean isSymlink(File file) throws IOException {
+        if (file == null)
+            throw new NullPointerException("File must not be null");
+        File canon;
+        if (file.getParent() == null) {
+            canon = file;
+        } else {
+            File canonDir = file.getParentFile().getCanonicalFile();
+            canon = new File(canonDir, file.getName());
+        }
+        return !canon.getCanonicalFile().equals(canon.getAbsoluteFile());
+    }
+}
